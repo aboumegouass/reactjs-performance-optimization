5091ad67b5eb6bd5a82d95a7a75ba3a984f821c5
aboumegouass <aboumegouass@gmail.com>
2025-02-12 16:59:34 +0100

    Removed `console.log`

--- src/components/CounterItem.tsx
+++ src/components/CounterItem.tsx
@@ -1,6 +1,5 @@
 import { memo } from "react";
 
 export const CounterItem = memo(({ count }: { count: number }) => {
-    console.log("CounterDisplay rendered");
     return <h2>Counter: {count}</h2>;
 });
